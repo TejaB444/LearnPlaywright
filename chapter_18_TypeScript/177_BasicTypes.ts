605c9479998b1d16382f76f844624a5dee22c635
Bhanu B <bhanubteja28@gmail.com>
2026-05-02 18:48:05 +0530

    Adding TypeScript examples and a package.json file.

---
+++ chapter_18_TypeScript/177_BasicTypes.ts
@@ -0,0 +1,20 @@
+//Primitive types
+
+let name : string ="John"
+let age:number = 30;
+let isactive: boolean = true;
+let nothing:null = null;
+let notdefined:undefined = undefined;
+
+
+//Arrays
+let numbers : number[] = [1,2,3,4,5];
+let names : Array<String> = ['John', 'Michael'];
+
+//Any(avoid when possible)
+let anything : any = 'hello'
+
+//unknown (safer than  any)
+let unknown:unknown = "Hello"
+
+
